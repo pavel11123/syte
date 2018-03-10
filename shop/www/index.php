6912450f3db27657175685b91d80834d674e9ef6
pavel11123 <korobko.11@inbox.ru>
2018-03-10 11:50:04 +0600

    Initial commit

---
+++ shop/www/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+
+	<title>Sport House</title>
+</head>
+
+<body>
+
+<!-- Основной блок -->
+<div id="block_body"> 
+<!-- Подключение папки include с верхним блоком -->
+<?php
+	include ("Z:/home/shop/www/include/block_header.php");
+?>
+<!-- Создание блоков для фотографий -->
+<div> Новый блок </div>
+<div id="block_image"></div>
+<div id="block_image1"></div>
+<div id="block_image2"></div>
+<div> Спорт </div>
+<div id="block_image3"></div>
+<div id="block_image4"></div>
+<div id="block_image5"></div>
+<div id="block_image6"></div>
+<div> Для всех</div>
+<div id="block_image7"></div>
+<div id="block_image8"></div>
+<div id="block_image9"></div>
+
+<!-- Подключение папки include с нижним блоком -->
+<?php
+	include ("Z:/home/shop/www/include/block_footer.php");
+?>
+</div>
+
+</body>
+</html>
