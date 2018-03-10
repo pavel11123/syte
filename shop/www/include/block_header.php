81b437de20318239aa81a43cbcd36d9285442b95
pavel11123 <korobko.11@inbox.ru>
2018-03-10 11:59:46 +0600

    start

---
+++ shop/www/include/block_header.php
@@ -0,0 +1,27 @@
+<!-- Создание верхнего блока -->
+<div id = "block_header">
+
+<!-- Создание логотипа -->
+<div id="block_logotyp">
+Sport House</div>
+
+<!-- Создание кнопки Регистрация -->
+<div id="block_regist">
+Регистрация </div>
+
+<!-- Создание кнопки Вход -->
+<ul id="block_login">
+Вход</ul>
+
+<!-- Создание ссылок в меню-->
+<ul id="block_menu">
+<li><a href="Footbal.php">Футбол</a></li>
+<li><a href="Basketbal.php">Баскетбол</a></li>
+<li><a href="Voleybal.php">Волейбол</a></li>
+<li><a href="Tennis.php">Теннис</a></li>
+<li><a href="Hockey.php">Хоккей</a></li>
+<li><a href="Athletic.php">Легкая атлетика</a></li>
+<li><a href="Novinki.php">Новинки</a></li>
+<li><a href="Sale.php">Акции</a></li>
+</ul>
+ </div>
